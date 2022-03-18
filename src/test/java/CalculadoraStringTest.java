7e8cb47530f57679ea35f17fbd5dbfae6159aa6b
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-03-18 18:45:35 -0300

    Se implemento los test para Los numeros separados por saltos de lineas, numeros delimitados de cualquier forma, numeros negativos, paranumeros mayores a mil y que no permitan delimitadores al principio

--- src/test/java/CalculadoraStringTest.java
+++ src/test/java/CalculadoraStringTest.java
@@ -22,4 +22,34 @@ public class CalculadoraStringTest {
         Assertions.assertEquals(3, CalculadoraStringInstance.add("2,1"));
         Assertions.assertEquals(6, CalculadoraStringInstance.add("2,4"));
     }
+
+    @Test
+    public void dosNumeroRetornaValoresSumadosSeparadosPorSaltoLineaTest() throws Exception {
+        Assertions.assertEquals(4, CalculadoraStringInstance.add("2\n2"));
+        Assertions.assertEquals(3, CalculadoraStringInstance.add("2\n1"));
+        Assertions.assertEquals(6, CalculadoraStringInstance.add("2\n4"));
+    }
+
+    @Test
+    public void dosNumeroRetornaValoresSumadosSeparadosPorCualquierCosaTest() throws Exception {
+        Assertions.assertEquals(11, CalculadoraStringInstance.add("2\n7,2"));
+        Assertions.assertEquals(6, CalculadoraStringInstance.add("3,2\n1"));
+        Assertions.assertEquals(7, CalculadoraStringInstance.add("2\n1,4"));
+    }
+    @Test
+    public void NumNegativoTest(){
+        Assertions.assertThrows(Exception.class, ()->{CalculadoraStringInstance.add("-1,-1");});
+    }
+    @Test
+    public void IgnoraSiEs1000Test() throws Exception {
+        Assertions.assertEquals(2, CalculadoraStringInstance.add("2,1002"));
+        Assertions.assertEquals(2, CalculadoraStringInstance.add("2,1100"));
+        Assertions.assertEquals(2, CalculadoraStringInstance.add("2,4000"));
+    }
+
+    @Test
+    public void SoloSePuedePonerCaracterDelimitadorEnElPrimeroTest() throws Exception {
+        Assertions.assertEquals(2, CalculadoraStringInstance.add("#2#1002"));
+    }
+
 }
